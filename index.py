481d6f1254860199011f149f74ba6d5be6367ff1
Ryan Lucas <ryan@pop-os.localdomain>
2022-09-09 00:39:03 -0300

    fix: translate functions

--- index.py
+++ index.py
@@ -1,19 +1,56 @@
-# crie um programa que leia data.csv e exiba seu índice e conteúdo na tela
-# dica: use enumerate()
-# dica: use open() e .readlines()
-# dica: use print() e .format()
-# dica: use for
-# dica: use split()
-import csv
-with open('data.csv', 'r') as f:
-    reader = csv.reader(f)
-    for i, row in enumerate(reader):
-        print('Linha {}: {}'.format(i, row))
-        # crie um arquivo .py para cada índice e escreva um comentário com seu conteúdo
-        # dica: use open() e .write()
-        # dica: use .format()
-        # dica: use open() e .close()
-        with open('{}.py'.format(i), 'w') as f2:
-            f2.write('# {}'.format(row))
-            f2.close()
-        i += 1
+import os
+import requests, json, csv
+import nltk 
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize, sent_tokenize 
+from deep_translator import GoogleTranslator
+
+# # crie um programa que leia data.csv e exiba seu índice e conteúdo na tela
+# # dica: use enumerate()
+# # dica: use open() e .readlines()
+# # dica: use print() e .format()
+# # dica: use for
+# # dica: use split()
+
+
+def translate(string):
+    return GoogleTranslator(source='auto', target='en').translate(string)
+
+
+
+# Write a function to summarize a array of strings using nltk
+def summarize(string):
+    # get the first 3 words of the summary
+    first_words = string.split(' ')[:5]
+    summary = ' '.join(first_words).replace(' ', '_').replace(',', '-')
+    return summary
+
+
+def writeFiles(filename):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter='\n')
+        for i, row in enumerate(reader):
+
+            print(row)
+            translated = translate(row[0])
+            filename = summarize(translated)
+
+            if len(filename) == 0:
+                return
+            filename.split('.').pop(0)
+            with open('./generated/{}.py'.format(filename), 'w') as f2:
+                f2.write('# {}'.format(translated))
+                f2.close()
+            i += 1
+
+
+def countFiles():
+    return len(os.listdir('./generated'))
+
+if __name__ == '__main__':
+    writeFiles('./data/index.csv')
+    print(countFiles())
+
+       
+
+
--- sucessor.py
+++
@@ -1,4 +0,0 @@
-# crie um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
-
-n = int(input('Digite um número: '))
-print('O número digitado foi {}, seu antecessor é {} e seu sucessor é {}'.format(n, n-1, n+1))
